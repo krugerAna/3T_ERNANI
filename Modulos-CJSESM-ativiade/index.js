50bb0b42cd990d9407460dd97ba0d27930716001
krugerAna <krugeranajulia@gmail.com>
2024-09-17 11:34:09 -0300

    modulosCJSESM

---
+++ Modulos-CJSESM-ativiade/index.js
@@ -0,0 +1,7 @@
+import { veiculo } from "./lib/veiculo.js";
+
+import { fs } from ".lib/modulo.cjs";
+
+console.log(veiculo());
+
+console.log(fs);
---
+++ Modulos-CJSESM-ativiade/veiculo.js
@@ -0,0 +1,10 @@
+function acelerar() {
+  return "10km/h"
+}"
+    marca: "volvo"
+    modelo: "xc90"
+    fabricante: "paiDaDuda"
+  };
+}
+
+export { vículo };
